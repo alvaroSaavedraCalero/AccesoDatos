e80dcd84ab8337cf50004211a05e525c5e40d2d8
Alvaro Saavedra <saavedra.calero.alvaro@gmail.com>
2022-11-11 17:46:56 +0100

    Haciendo no sabe ni ella que con objetos

--- src/serializarObjetos/Main.java
+++ src/serializarObjetos/Main.java
@@ -1,13 +1,21 @@
 package serializarObjetos;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	public static void escribir() {
 		File fichero = new File("ruta");
 		ObjectOutputStream objectOutputStream;
 		try {
@@ -21,8 +29,29 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static void leer() {
+		File fichero = new File("ruta");
+		ObjectInputStream objectInputStream;
+		try {
+			objectInputStream = new ObjectInputStream(new FileInputStream(fichero));
+			Object object = objectInputStream.readObject();
+			
+			while (object != null) {
+				if (object instanceof Persona) {
+					System.out.println(object.toString());
+					objectInputStream.readObject();
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
-
 }
